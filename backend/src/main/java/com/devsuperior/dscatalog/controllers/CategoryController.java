93b54b4aee7e5c5a840118b7b46c6542235d52ab
Raul Vitorio <raulvitorio790@gmail.com>
2026-04-27 14:06:49 -0300

    Delete

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -39,5 +39,10 @@ public class CategoryController {
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.controllers.exceptions;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -21,5 +22,15 @@ public class ControllerExceptionHandler {
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Database Exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
