c390b969e7fac2660442e45da1a396bd566e75fe
Raul Vitorio <raulvitorio790@gmail.com>
2026-04-28 13:21:05 -0300

    Product repository, service, controller, GET ok

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,109 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    private Instant date;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO() {}
+
+    public ProductDTO(Instant date, String imgUrl, Double price, String description, String name, Long id) {
+        this.date = date;
+        this.imgUrl = imgUrl;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+        this.id = id;
+    }
+    public ProductDTO(Product entity) {
+        this.date = entity.getDate();
+        this.imgUrl = entity.getImgUrl();;
+        this.price = entity.getPrice();;
+        this.description = entity.getDescription();
+        this.name = entity.getName();;
+        this.id = entity.getId();
+    }
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(x -> this.categories.add(new CategoryDTO(x)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDTO that = (ProductDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
